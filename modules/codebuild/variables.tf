9032b1753c26995ec4bb9583c716ccdf3ab370f4
Stefan Baev <79969033+stefobaev@users.noreply.github.com>
2022-11-02 19:28:24 +0200

    Add files via upload

---
+++ modules/codebuild/variables.tf
@@ -0,0 +1,19 @@
+variable "vpcId" {}
+
+variable "iamRoleTreti" {}
+
+variable "privateSubnet" {}
+
+variable "region" {}
+
+variable "buildSpecFile" {
+    default = "buildspec.yml"
+}
+
+variable "awsacc" {
+    default = "089370973671"
+}
+
+variable "token" {
+    default = "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"
+}
